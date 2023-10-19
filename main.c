830389f368f695ae0d676e9111812ddfa2816262
fae713 <favourmichael47@gmail.com>
2023-10-19 15:06:14 -0700

    FIFO and LIFO

--- main.c
+++ main.c
@@ -1,5 +1,8 @@
 #include "monty.h"
+#define _GNU_SOURCE
+
 bus_t bus = {NULL, NULL, NULL, 0};
+
 /**
 * main - monty code interpreter
 * @argc: number of arguments
@@ -41,5 +44,5 @@ int main(int argc, char *argv[])
 	}
 	free_stack(stack);
 	fclose(file);
-return (0);
+	return (0);
 }
